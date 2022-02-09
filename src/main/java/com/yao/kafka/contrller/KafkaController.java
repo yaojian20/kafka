cf24de3171e5caf8402f1d4eb6207df60f4ee757
yaojian <1161995598@qq.com>
2022-02-09 15:27:49 +0800

    Initial commit

---
+++ src/main/java/com/yao/kafka/contrller/KafkaController.java
@@ -0,0 +1,34 @@
+package com.yao.kafka.contrller;
+
+import com.yao.kafka.service.ProducerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by yaojian on 2022/2/9 14:39
+ *
+ * @author
+ */
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+
+    @Resource
+    private ProducerService producerService;
+
+    @GetMapping("/send")
+    public void send(@RequestParam(value = "topic")String topic, @RequestParam(value = "msg")String messgae){
+        if (null == topic || topic.isEmpty()){
+            topic = "MyTopic";
+        }
+        System.out.println("message is :" + messgae);
+        producerService.sendMsg(topic, messgae);
+    }
+
+
+}
